862cd9acd7a8531f3364805bc8855dd79982f4fd
litao <tli66@best-inc.com>
2019-07-10 12:47:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# fis3-parser-babel-best
+
+拷贝了 `fis3-parser-babel-imweb` 代码，将对 `babel-core` 的依赖移除，转为依赖项目中的 `@babel/core`，便于升级。
+
+## Usage
+
+```bash
+yarn add fis3-parser-babel-best -D
+```
---
+++ src/index.js
@@ -0,0 +1,66 @@
+import babel from '@babel/core';
+
+import cache from './cache';
+
+/* eslint-disable no-param-reassign */
+
+/**
+ * interface File {
+ *   subpath: string;
+ *   useBabel: boolean;
+ *   ext: string;
+ * }
+ * interface Config {
+ *   sourceMapRelative: boolean;
+ * }
+ * @param {string} content - 要编译的文件内容
+ * @param {File} file
+ * @param {Config} conf
+ * @return {string}
+ */
+export default function transform(content, file, conf) {
+  if (file.useBabel === false) {
+    return content;
+  }
+
+  const md5 = cache.createMd5(content);
+  const cachedContent = cache.getCachedContent(md5);
+  if (cachedContent) {
+    return cachedContent;
+  }
+
+  // 添加 jsx 的 html 语言能力处理
+  if (fis.compile.partial && file.ext === '.jsx') {
+    content = fis.compile.partial(content, file, {
+      ext: '.html',
+      isHtmlLike: true,
+    });
+  }
+
+  const { sourceMapRelative } = conf;
+
+  if (sourceMapRelative) {
+    delete conf.sourceMapRelative;
+  }
+
+  // 出于安全考虑，不使用原始路径
+  conf.filename = file.subpath.substring(1);
+  const result = babel.transform(content, conf);
+
+  // 添加resourcemap输出
+  if (result.map) {
+    const mapping = fis.file.wrap(`${file.dirname}/${file.filename}${file.rExt}.map`);
+    mapping.setContent(JSON.stringify(result.map, null, 4));
+    const url = sourceMapRelative
+      ? (`./${file.basename}.map`).replace('jsx', 'js')
+      : mapping.getUrl(fis.compile.settings.hash, fis.compile.settings.domain);
+    result.code = result.code.replace(/\n?\s*\/\/#\ssourceMappingURL=.*?(?:\n|$)/g, '');
+    result.code += `\n//# sourceMappingURL=${url}\n`;
+    file.extras = file.extras || {};
+    file.extras.derived = file.extras.derived || [];
+    file.extras.derived.push(mapping);
+  }
+
+  cache.cacheContent(md5, result.code);
+  return result.code;
+}
